3d01ac6b65a8d34541ebc9571ed57ae7b6b615d9
Keith Wolcott <wolcott.keith@gmail.com>
2017-12-05 09:31:24 -0600

    Check if Internet again

--- app/src/main/java/com/example/owner/tabsexample2/Log_In.java
+++ app/src/main/java/com/example/owner/tabsexample2/Log_In.java
@@ -1,6 +1,10 @@
 package com.example.owner.tabsexample2;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -46,11 +50,39 @@ public class Log_In extends AppCompatActivity implements AsyncResponse, Serializ
                 // get the entered values for username and password
                 userName = usrName_Btn.getText().toString();
                 pswd = pswd_Btn.getText().toString();
-                checkCredentials(userName,pswd);
+                if (isConnected()) {
+                    // get the entered values for username and password
+                    userName = usrName_Btn.getText().toString();
+                    pswd = pswd_Btn.getText().toString();
+                    checkCredentials(userName,pswd);
+                }
+                else {
+                    alertMsg("Error","You need to be connected to the internet to log in.");
+                }
 
         }});
 
 
+    }
+
+    private boolean isConnected() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+    private void alertMsg(String title, String msg) {
+        AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+        alertDialog.setTitle(title);
+        alertDialog.setMessage(msg);
+        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        alertDialog.show();
     }
 
     private void checkCredentials(String userName, String password) {
